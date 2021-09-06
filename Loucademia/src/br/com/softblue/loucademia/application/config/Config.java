ac9bcd66b343b8d1cbc7cd7a0c3d107f9ecf8e87
lucas <lucas@LAPTOP-3AA3T9LB.mshome.net>
2021-09-06 16:58:33 -0300

    tok

--- Loucademia/src/br/com/softblue/loucademia/application/config/Config.java
+++ Loucademia/src/br/com/softblue/loucademia/application/config/Config.java
@@ -3,7 +3,7 @@ package br.com.softblue.loucademia.application.config;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
 import javax.faces.annotation.FacesConfig.Version;;
-
+//t
 @FacesConfig(version = Version.JSF_2_3)
 @ApplicationScoped
 public class Config {
